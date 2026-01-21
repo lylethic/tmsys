06154598bffb1cd24307a53da30398b5f7f51375
lylethic <lely0168340@gmail.com>
2026-01-21 22:04:09 +0700

    fix(popup): convert date

--- Domain/Entities/popup.cs
+++ Domain/Entities/popup.cs
@@ -8,15 +8,13 @@ public partial class Popup : SystemLogEntity<Guid>
 {
     public string Content { get; set; } = null!;
 
-    public DateOnly Validity_start { get; set; }
+    public DateTime Validity_start { get; set; }
 
-    public DateOnly Validity_end { get; set; }
+    public DateTime Validity_end { get; set; }
 
     public short Type { get; set; }
 
     public DateTime? Display_from { get; set; }
 
     public DateTime? Display_to { get; set; }
-
-    public bool Is_active { get; set; }
 }
